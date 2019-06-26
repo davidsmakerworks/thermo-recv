7d8f87369b5c434b5b3bd29635fd9097a0b5d903
David Rice <11968726+davidsmakerworks@users.noreply.github.com>
2019-06-25 21:19:57 -0700

    Update for compatibility with restructured driver

--- ThermoRecv.X/main.c
+++ ThermoRecv.X/main.c
@@ -92,6 +92,7 @@
 #include <stdbool.h>
 
 #include "nRF24L01P.h"
+#include "nRF24L01P-cfg.h"
 
 #ifndef RF_CHANNEL
 #error RF_CHANNEL must be defined
@@ -404,7 +405,7 @@ void main(void) {
     init_mssp();
     init_rf();
     
-    NRF24_CE = 1;
+    NRF24_CE_ACTIVE();
     
     all_off();
     update_needed = true;
